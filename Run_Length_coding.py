7c47eface78ca7f3e0c1d70ff5b401913d466a3d
VivekRedD1999 <58820599+VivekRedD1999@users.noreply.github.com>
2020-06-21 01:23:29 +0530

    Update Run_Length_coding.py

--- Run_Length_coding.py
+++ Run_Length_coding.py
@@ -1,3 +1,7 @@
+#    Run-length encoding is a fast and simple method of encoding strings. 
+#    The basic idea is to represent repeated successive characters as a single count and character
+#    For example, the string "AAAABBBCCDAA" would be encoded as "4A3B2C1D2A".
+#=====================================================================================================
 def Run_Length_Coding(x):
     result, final = [], ""
     count = 1
